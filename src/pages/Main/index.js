8a4c6b1e566a54dade85d3985181e549009de454
Diego Collares <diego.collares@gmail.com>
2019-07-10 11:29:13 -0300

    Styled component, added react-native-vector-icons and configured libs with link native

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,17 +1,28 @@
 import React from 'react';
-import { View, Text } from 'react-native';
-import { Container } from './style';
+
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import { Container, Form, Input, SubmitButton  } from './style';
 
 const Main = () => {
   return (
     <Container>
-      <Text>Hello Main!</Text>
+      <Form>
+        <Input
+          autoCorrect={false}
+          autoCapitalize="none"
+          placeholder="Adicionar usuário"
+        />
+        <SubmitButton>
+          <Icon name="add" size={20} color="#fff" />
+        </SubmitButton>
+      </Form>
     </Container>
   );
 };
 
 Main.navigationOptions = {
-  title: 'Principal'
-}
+  title: 'Principal',
+};
 
 export default Main;
